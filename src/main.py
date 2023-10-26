c0170e8daede9fc65ff022a0765d380d01a83a03
scpapa21 <scalderonpapa@gmail.com>
2023-10-26 12:22:48 -0500

    Add Analysis Results

--- src/main.py
+++ src/main.py
@@ -6,8 +6,5 @@ insurance_file = pathlib.Path(__file__).parent.parent / 'data' / 'insurance.csv'
 insurance_df = pd.read_csv(insurance_file)
 
 patients = pt.Patients(insurance_df)
-
-print(patients.age_analysis())
-print(patients.sex_analysis())
-print(patients.smoker_analysis())
+patients.analysis_results()
 
--- src/patients.py
+++ src/patients.py
@@ -13,15 +13,15 @@ class Patients:
         self.patients_dictionary = {patient_id : values for patient_id, values in enumerate(zip(self.ages, self.sex, self.bmi, self.children, self.smoker, self.region, self.insurance_cost))}
 
         self.__create_by_sex_dict()
-        self.__calc_total_patients()
         self.__get_unique_regions()
+        self.__calc_total_patients()
     
 
     # Self Calculations Methods
     def __calc_total_patients(self):
         self.total_patients = len(self.ages)
-        self.total_male = len(self.ages)
-        self.total_female = len(self.ages)
+        self.total_male = len(self.by_sex_dict['male']['age'])
+        self.total_female = len(self.by_sex_dict['female']['age'])
 
     def __create_by_sex_dict(self):
         by_sex_dict =  {'male': {'age' : [], 'bmi' : [], 'children' : [], 'smoker' : [], 'region' : [], 'insurance_cost' : []}, 
@@ -44,9 +44,11 @@ class Patients:
 
     # Data description
     def data_description(self):
+        print('\nData Description:')
         return self.df.describe()
     
     def data_correlation(self):
+        print('\nData Correlation:')
         df_updated = self.df.copy(True)
 
         df_updated.loc[df_updated['sex'] == 'male', 'sex'] = 1
@@ -60,14 +62,43 @@ class Patients:
         return df_updated.corr()
     
 
+    # Base Analysis
+    def base_analysis(self):
+        result = '\nBase Analysis:\n'
+
+        average_bmi = round(self.__calc_average_bmi(),2)
+        average_children = round(self.__calc_average_children(),2)
+        average_cost = round(self.__calc_average_cost(),2)
+
+        result += f'There are {self.total_patients} total patients, {self.total_male} male and {self.total_female} female.\n'
+        result += f'In average they have {average_children} children, {average_bmi} bmi and ${average_cost} insurance cost;'
+        
+        return result
+    
+    def __calc_average_bmi(self):
+        return sum(self.bmi) / len(self.bmi)
+
+    def __calc_average_children(self):
+        return sum(self.children) / len(self.children)
+    
+    def __calc_average_cost(self):
+        return sum(self.insurance_cost) / len(self.insurance_cost)
+
+
     # Age Analysis
     def age_analysis(self):
+        result = '\nAge Analysis:\n'
+
         age_average = round(self.__calc_age_average(),2)
         male_age_average = round(self.__calc_age_average_by_sex('male'),2)
         female_age_average = round(self.__calc_age_average_by_sex('female'),2)
         age_cost_correlation = round(self.__calc_age_cost_correlation(),3)
 
-        return f'\nAge Analysis:\nThe average age of the patients is {age_average}.\nFor males is {male_age_average} and for females is {female_age_average}.\nThe correlation between age and the insurance cost is {age_cost_correlation}.'
+        result += f'The average age of the patients is {age_average}.\n'
+        result += f'For males is {male_age_average} and for females is {female_age_average}.\n'
+        result += f'The correlation between age and the insurance cost is {age_cost_correlation}.'
+
+        return result
     
     def __calc_age_average(self):
         return sum(self.ages)/self.total_patients
@@ -82,6 +113,8 @@ class Patients:
     
     # Sex Analysis
     def sex_analysis(self):
+        result = '\nSex Analysis:\n'
+
         male_children_average = round(self.__calc_average_children_by_sex('male'),2)
         female_children_average = round(self.__calc_average_children_by_sex('female'),2)
 
@@ -94,7 +127,13 @@ class Patients:
         male_insurance_cost_correlation = round(self.__calc_sex_cost_correlation('male'),3)
         female_insurance_cost_correlation = round(self.__calc_sex_cost_correlation('female'),3)
 
-        return f'\nSex Analysis:\nAverage number of children for males is {male_children_average}, for females is {female_children_average}.\nThe average bmi for males is {male_bmi_average}, for females is {female_bmi_average}.\nAverage insurance cost for males is ${male_insurance_cost_average}, for females is ${female_insurance_cost_average}.\nThe correlation between being a male and the insurance cost is {male_insurance_cost_correlation}, while the correlation between being a female and the insurance cost is {female_insurance_cost_correlation}.'
+        result += f'Average number of children for males is {male_children_average}, for females is {female_children_average}.\n'
+        result += f'The average bmi for males is {male_bmi_average}, for females is {female_bmi_average}.\n'
+        result += f'Average insurance cost for males is ${male_insurance_cost_average}, for females is ${female_insurance_cost_average}.\n'
+        result += f'The correlation between being a male and the insurance cost is {male_insurance_cost_correlation}.\n'
+        result += f'While the correlation between being a female and the insurance cost is {female_insurance_cost_correlation}.'
+
+        return result
 
     def __calc_total_by_sex(self, sex : str):
         return self.sex.value_counts()[sex]
@@ -129,6 +168,8 @@ class Patients:
 
     # Smoker Analysis
     def smoker_analysis(self):
+        result = '\nSmoker Analysis:\n'
+
         male_smokers = self.__calc_smokers_by_sex('male')
         female_smokers = self.__calc_smokers_by_sex('female')
 
@@ -136,11 +177,15 @@ class Patients:
         female_smokers_percentage = round(self.__calc_smoker_percentage_by_sex(female_smokers, 'female'),4)*100
 
         smoker_cost_correlation = round(self.__calc_smoker_cost_correlation(),3)
-
         smoker_average_cost = round(self.__calc_average_insurance_cost_if_smoker(True),2)
         non_smoker_average_cost = round(self.__calc_average_insurance_cost_if_smoker(False),2)
 
-        return f'\nSmoker Analysis:\nThere are {male_smokers} male smokers, this represents {male_smokers_percentage}% of males.\nFor females there are {female_smokers} smokers that represent {female_smokers_percentage}% of females.\nThe correlation between smoking and the insurance cost is {smoker_cost_correlation}.\nThis is evident in the average insurance cost of a smoker: ${smoker_average_cost} compared to the insurance cost of a non smoker: ${non_smoker_average_cost}.'
+        result += f'There are {male_smokers} male smokers, this represents {male_smokers_percentage}% of males.\n'
+        result += f'For females there are {female_smokers} smokers, that represent {female_smokers_percentage}% of females.\n'
+        result += f'The correlation between smoking and the insurance cost is {smoker_cost_correlation}.\n'
+        result += f'This is evident in the average insurance cost of a smoker: ${smoker_average_cost} compared to the insurance cost of a non smoker: ${non_smoker_average_cost}.'
+        
+        return result
 
     def __calc_smokers_by_sex(self, sex : str):
         smoker_count = self.by_sex_dict[sex]['smoker'].count('yes')
@@ -182,8 +227,7 @@ class Patients:
 
     # Region Analysis
     def regions_analysis(self):
-        print('\nRegions Analysis:')
-        result = ''
+        result = '\nRegions Analysis:\n'
 
         for region in self.unique_regions:
             smokers_in_region = self.__calc_smokers_by_region(region)
@@ -191,7 +235,7 @@ class Patients:
             result += f'In the {region} there are {smokers_in_region} smokers. The average cost of insurance in this region is of ${cost_in_region}.\n'
 
         return result
-
+    
     def __calc_smokers_by_region(self, region_used : str):
         smokers_by_region_dict = {region : [] for region in self.unique_regions}
         regions_smoker_info = zip(self.region, self.smoker)
@@ -210,5 +254,18 @@ class Patients:
             cost_by_region_dict[region].append(cost)
 
         return sum(cost_by_region_dict[region_used]) / len(cost_by_region_dict[region_used])
+
+
+    # Complete Analysis
+    def analysis_results(self):
+        print(self.base_analysis())
+        print(self.age_analysis())
+        print(self.sex_analysis())
+        print(self.smoker_analysis())
+        print(self.regions_analysis())
+
+        print(self.data_description())
+        print(self.data_correlation())
+    
     
 
